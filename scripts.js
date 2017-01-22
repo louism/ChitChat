a9bceabbbb8790c6a09f134493b4a661eccf341c
Alex Goncalves <agoncalves4141@gmail.com>
2017-01-22 17:03:54 -0500

    Username input added, fixed bugs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
-/*var name = "";
-
-function getName(){
-	name = prompt("Please enter your name");
-}*/
-
 // Globals
+var name = "";
 var nodes = document.getElementsByClassName("chatSubmit");
 var button = nodes[0];
 var audio = new Audio('./ding.mp3');
@@ -14,10 +9,6 @@ var audio = new Audio('./ding.mp3');
 // Socket-Related Functionality
 var socket = new WebSocket('ws://localhost:8001');
 
-socket.onopen = function(){
-	socket.send(name + " has joined the room");
-}
-
 socket.onmessage = function(evt){
 	audio.play();
 	$('<div class="yourSpace"><li class="foreign">' + evt.data.substring(1,evt.data.length-1) + '</li></div>').hide().appendTo('.messages').fadeIn(600);
@@ -26,7 +17,7 @@ socket.onmessage = function(evt){
 button.onclick = function(){
 	var message = document.getElementById('chatSet').value; 
 	$('<div class="mySpace"><li class="local">' + message + '</li></div>').hide().appendTo('.messages').fadeIn(600);
-	socket.send(/*name +*/ " said: " + message);
+	socket.send(name + " said: " + message);
 	document.getElementById('chatSet').value = '';
 	updateScroll();
 }
@@ -40,7 +31,7 @@ $(document).keypress(function(e){
 		var message = document.getElementById('chatSet').value;
 		if(message != ''){
 			$('<div class="mySpace"><li class="local">' + message + '</li></div>').hide().appendTo('.messages').fadeIn(600);
-			socket.send(/*name +*/ " said: " + message);
+			socket.send(name + " said: " + message);
 			document.getElementById('chatSet').value = '';
 			updateScroll();
 		}
@@ -53,3 +44,13 @@ $(document).keypress(function(e){
 function updateScroll(){
 	window.scrollTo(0,document.body.scrollHeight);
 }
+
+$('.modal-button').click(function(){
+	var modal = document.getElementById('mod-input').value;
+	if(modal != ''){
+		name = modal;
+		$('.modal-form').remove();
+		socket.send(name + ' has joined the room');
+		$('.footer').css("visibility", "visible");
+	}
+});
